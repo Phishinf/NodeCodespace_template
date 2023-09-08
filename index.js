1d43c9530c65526d1f6209597af167a6d072fb8e
Victorius Hung <135007842+Phishinf@users.noreply.github.com>
2023-09-08 11:54:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
-const haikus = require('./haikus.json');
+const haikus = require('./victor.json');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
